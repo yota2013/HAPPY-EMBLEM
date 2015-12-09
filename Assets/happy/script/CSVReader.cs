facc991c7bc45b1fd0983f48e3092b9420fa2c95
happy2042 <is0228he@ed.ritsmei.ac.jp>
2015-12-09 20:32:22 +0900

    CSV読み込むクラス作るだけ作った（未実装）

---
+++ Assets/happy/script/CSVReader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CSVReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
