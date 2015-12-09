b3b06cb94b4a6eeecaff08ad7510e0d5aa117e28
happy2042 <is0228he@ed.ritsmei.ac.jp>
2015-12-09 20:58:37 +0900

    CSV読み込むメソッド名変更、中身ちょろっと書いた

---
+++ Assets/happy/script/CSVLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CSVLoader : MonoBehaviour {
+
+	public void CSVLoad(string filePath){
+		TextAsset csv = Resources.Load(filePath) as TextAsset;
+		StringReader reader = new StringReader(csv.text);
+		while (reader.Peek() > -1) {
+			string line = reader.ReadLine();
+			string[] values = line.Split(',');
+		}
+	}
+}
