e67d4614bbf049b2b0f98ed43c594a8c9345d494
yuuki Tada <is0220ii@ed.ristumei.ac.jp>
2015-12-12 23:33:45 +0900

    タイトル画面作成

---
+++ Assets/yota/Scripts/FadeMoveScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeMoveScene : MonoBehaviour {
+	public void MoveScene(string SceneName,float time)
+	{
+		FadeManager.Instance.LoadLevel (SceneName,time);
+	}
+
+}
