1a282d6c8d17d0bb2b86b170971dcbf7f8dcc425
happy2042 <is0228he@ed.ritsmei.ac.jp>
2015-12-12 20:36:49 +0900

    csvファイルのエンコード直した、あとCSV読み込む奴も編集した

--- Assets/happy/script/CSVLoader.cs
+++ Assets/happy/script/CSVLoader.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 using System.IO;
 
-public class CSVLoader : MonoBehaviour {
+public class CSVLoader : MonoBehaviour{
 
-	public void CSVLoad(string filePath){
-		TextAsset csv = Resources.Load(filePath) as TextAsset;
+	void Start() {
+		Debug.Log ("start");
+		CSVLoad ("TestUnitData");
+	}
+
+	public void CSVLoad(string filename){
+		TextAsset csv = Resources.Load("CSV/" + filename) as TextAsset;
 		StringReader reader = new StringReader(csv.text);
 		while (reader.Peek() > -1) {
 			string line = reader.ReadLine();
 			string[] values = line.Split(',');
+			Debug.Log (line);
+			if(values[0] == "UnitName"){
+				continue;
+			}else{
+				UnitStatusSetter (values);
+			}
 		}
+	}
+
+	public void UnitStatusSetter(string[] values) {
+		UnitStatus unitStatus = this.gameObject.GetComponent<UnitStatus> ();
+
+		unitStatus.Name = values [0];
+		unitStatus.Type = values [1];
+		unitStatus.AP 	= int.Parse(values [2]);
+		unitStatus.HP 	= int.Parse(values [3]);
+		unitStatus.DP 	= int.Parse(values [4]);
 	}
 }
